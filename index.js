a17a01cc8595ef108a7c662e509b0d79399cdc45
Debasish Debnath <ddebnath241@gmail.com>
2025-02-24 10:25:45 +0530

    update

--- index.js
+++ index.js
@@ -6,20 +6,38 @@ const port = process.env.PORT;
 app.get('/',(req, res) => {
     res.send('Hello, World!');
 })
+// get a list of 5 jokes
+app.get('/jokes',(req,res)=>{
+    const jokes =[
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content:'This is a another joke'
+        },
+        {
+            id:3,
+            title:'Another first joke',
+            content:'This is a another first joke'
+        },
+        {
+            id:4,
+            title:'Another second joke',
+            content:'This is a another second joke'
+        },
+        {
+            id:5,
+            title:'Another third joke',
+            content:'This is a another third joke'
+        },
+    ]
+    res.send(jokes);
+})
 
-app.get('/login',(req,res)=>{
-    res.send('Login Page');
-})
-app.get('/github',(req,res)=>{
-    res.send('Github Page');
-})
-app.get('/loginn',(req,res)=>{
-    res.send('Login Page');
-})
-
-app.get("/signup",(req,res)=>{
-    res.send(`<h1>Sign Up</h1>`);
-})
 app.listen(process.env.PORT,()=>{
     console.log(`Example app listening on port ${port}`);
 });
